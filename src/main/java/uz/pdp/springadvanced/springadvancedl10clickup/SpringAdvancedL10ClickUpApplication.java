f3deaed1b882d7bc3fe48a82e274be6f5274d821
NodiraEgamberdiyeva <n.egamberdiyeva017@gmail.com>
2022-07-08 09:52:09 +0500

    creating project

---
+++ src/main/java/uz/pdp/springadvanced/springadvancedl10clickup/SpringAdvancedL10ClickUpApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springadvanced.springadvancedl10clickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedL10ClickUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedL10ClickUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/springadvanced/springadvancedl10clickup/SpringAdvancedL10ClickUpApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springadvanced.springadvancedl10clickup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedL10ClickUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
